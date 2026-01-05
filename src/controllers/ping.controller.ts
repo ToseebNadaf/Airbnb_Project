bca10c802714f75df26cde549105dd1f54e32b0d
ToseebNadaf <toseebnadaf@gmail.com>
2026-01-06 00:57:00 +0530

    Added custom error handling

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,16 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import fs from "fs";
+import { NotFoundError } from "../utils/errors/app.error";
 
-export const pingHandler = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "pong" });
+export const pingHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    fs.readFileSync("sample");
+    res.status(200).json({ message: "pong" });
+  } catch (error) {
+    throw new NotFoundError("File not found....");
+  }
 };
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { serverConfig } from "./config/index";
 import v1Router from "./routers/v1/index.router";
 import v2Router from "./routers/v2/index.router";
+import { genericErrorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1", v1Router);
 app.use("/api/v2", v2Router);
+
+app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server is running on port ${serverConfig.PORT}`);
