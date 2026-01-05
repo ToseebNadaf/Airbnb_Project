ec8645067efbd7df3dcd0276e008e1ce3ca5c96a
ToseebNadaf <toseebnadaf@gmail.com>
2026-01-05 22:22:52 +0530

    Fixed minor errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { serverConfig } from "./config/index.js";
-import v1Router from "./routers/v1/index.router.js";
-import v2Router from "./routers/v2/index.router.js";
+import { serverConfig } from "./config/index";
+import v1Router from "./routers/v1/index.router";
+import v2Router from "./routers/v2/index.router";
 
 const app = express();
 app.use(express.json());
