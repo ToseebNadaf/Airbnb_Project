f45a6dff18879d8d0471fad2391866d10e30a095
ToseebNadaf <toseebnadaf@gmail.com>
2026-01-06 10:12:50 +0530

    Added loggers to track request cycle

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import { serverConfig } from "./config/index";
 import v1Router from "./routers/v1/index.router";
 import v2Router from "./routers/v2/index.router";
 import { genericErrorHandler } from "./middlewares/error.middleware";
+import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
 
 const app = express();
 app.use(express.json());
+
+app.use(attachCorrelationIdMiddleware);
 
 app.use("/api/v1", v1Router);
 app.use("/api/v2", v2Router);
