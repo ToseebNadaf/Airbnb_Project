e64a724404e810055e504d3eab78e0bc3aa76429
ToseebNadaf <toseebnadaf@gmail.com>
2026-01-06 10:24:34 +0530

    Made minor changes

--- src/middlewares/correlation.middleware.ts
+++ src/middlewares/correlation.middleware.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
 import { asyncLocalStorage } from "../utils/helpers/request.helpers";
+import { randomUUID } from "crypto";
 
 export const attachCorrelationIdMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const correlationId = uuidv4();
+  const correlationId = randomUUID();
   req.headers["x-correlation-id"] = correlationId;
 
   asyncLocalStorage.run({ correlationId: correlationId }, () => {
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import v1Router from "./routers/v1/index.router";
 import v2Router from "./routers/v2/index.router";
 import { genericErrorHandler } from "./middlewares/error.middleware";
 import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
+import logger from "./config/logger.config";
 
 const app = express();
 app.use(express.json());
@@ -16,5 +17,5 @@ app.use("/api/v2", v2Router);
 app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
-  console.log(`Server is running on port ${serverConfig.PORT}`);
+  logger.info(`Server is running on port ${serverConfig.PORT}`);
 });
